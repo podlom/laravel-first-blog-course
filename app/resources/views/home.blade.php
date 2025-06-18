00b73e1944501d1e81d9e8e8f10b58a19dafb2ee
Taras Shkodenko <taras.shkodenko@gmail.com>
2025-06-18 12:12:00 +0300

    Lesson 03: Перший маршрут, контролер і шаблон для Laravel

---
+++ app/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home | Your First Laravel Blog</title>
+</head>
+<body>
+    <h1>Welcome to your Laravel Blog</h1>
+    <p>This is your first custom page 🎉</p>
+</body>
+</html>

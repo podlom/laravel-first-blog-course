31b553561018d165d35739ee6fc892b110e60e9d
Taras Shkodenko <podlom@users.noreply.github.com>
2025-07-10 14:26:50 +0300

    Update web.php
    Added new routes to web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -20,5 +20,7 @@ Route::get('/contact', [PageController::class, 'contact']);
 
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
